86052a4cfdd66a3b7602f6081925dbf3822dec89
Celestial Phineas <hanghang0713@hotmail.com>
2017-12-30 04:31:19 +0000

    run.js

--- run.js
+++ run.js
@@ -1,15 +1,5 @@
-var http = require('http');
-var fs = require('fs');
-
-const PORT=8080; 
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
+var connect = require('connect');
+var serveStatic = require('serve-static');
+connect().use(serveStatic(__dirname)).listen(8080, function(){
+    console.log('Server running on 8080...');
 });
