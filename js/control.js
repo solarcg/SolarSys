c548fde39845cc66e3308c5f4ad3443188a4082c
wsl <96486d9b@gmail.com>
2019-06-17 23:06:00 +0800

    image compress

--- js/control.js
+++ js/control.js
@@ -10,18 +10,14 @@ function onWindowMouseMove(event) {
     // Keep the value in 0 -- 2 PI
     var body = params.Camera;
     if (mouseStatus.leftDown) {
-        if (trackCamera[body].theta > 2 * Math.PI) {
-            trackCamera[body].theta
-                = trackCamera[body].theta
-                - 2 * Math.PI * Math.floor(trackCamera[body].theta / (2 * Math.PI));
-        }
-        if (trackCamera[body].phi > 2 * Math.PI) {
-            trackCamera[body].phi
-                = trackCamera[body].phi
-                - 2 * Math.PI * Math.floor(trackCamera[body].phi / (2 * Math.PI));
-        }
+        trackCamera[body].theta = trackCamera[body].theta % (2 * Math.PI);
+        trackCamera[body].phi = trackCamera[body].phi % (0.5 * Math.PI);
+        
         trackCamera[body].theta += (event.clientX - windowHalfX - mouseStatus.x) * 0.01;
-        trackCamera[body].phi += (event.clientY - windowHalfY - mouseStatus.y) * 0.01;
+
+        if (trackCamera[body].phi + (event.clientY - windowHalfY - mouseStatus.y) * 0.01 >= -0.5 * Math.PI &&
+            trackCamera[body].phi + (event.clientY - windowHalfY - mouseStatus.y) * 0.01 <= 0.5 * Math.PI)
+            trackCamera[body].phi += (event.clientY - windowHalfY - mouseStatus.y) * 0.01;
     }
     mouseStatus.x = event.clientX - windowHalfX;
     mouseStatus.y = event.clientY - windowHalfY;
@@ -70,11 +66,11 @@ function onMouseWheelChange(event) {
     trackCamera[body].distance = newDistance;
 }
 
-var posSrc = {pos: 0.0};
+var posSrc = { pos: 0.0 };
 var oX, oY, oZ, dX, dY, dZ, oTheta, dTheta, oPhi, dPhi, oDistance, dDistance, oSafeDis, dSafeDis;
 var oCX, oCY, oCZ, dCX, dCY, dCZ;
 tween = new TWEEN.Tween(posSrc)
-    .to({pos: 1.0}, 4000)
+    .to({ pos: 1.0 }, 4000)
     .easing(TWEEN.Easing.Quartic.InOut)
     .onUpdate(function () {
         var pos = posSrc.pos;
